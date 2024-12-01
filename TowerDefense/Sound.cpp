f9d86ebaa240094eabbb8bc6294dd12895f0b0e5
Nguyen Van Hop <163983884+hoopyTL@users.noreply.github.com>
2024-12-01 09:42:05 +0700

    Update Sound.cpp

--- TowerDefense/Sound.cpp
+++ TowerDefense/Sound.cpp
@@ -1,1 +1,12 @@
 #include "Sound.h"
+
+void Sound::playSound(int i)
+{
+	/*static vector<const wchar_t*> soundFile{ L"Sound\\MOVE.wav", L"Sound\\enterbutton.wav", L"Sound\\error.wav", L"Sound\\enter.wav", L"Sound\\win.wav",  L"Sound\\SoundGame.wav", L"Sound\\lose.wav", L"Sound\\nhacnen.wav" };
+	PlaySound(soundFile[i], NULL, SND_FILENAME | SND_ASYNC);*/
+}
+
+void Sound::stopSound()
+{
+	/*PlaySound(NULL, NULL, SND_FILENAME | SND_ASYNC);*/
+}
